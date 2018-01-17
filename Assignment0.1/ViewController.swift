4bdab66273e1842908f3c63f889548ea54076604
Jicang Wang <s3479320@w8408156.cs.rmit.edu.au>
2018-01-17 17:24:26 +1100

    The first upload

---
+++ Assignment0.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment0.1
+//
+//  Created by Jicang Wang on 1/16/18.
+//  Copyright © 2018 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

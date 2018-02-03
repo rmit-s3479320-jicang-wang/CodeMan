55f9f996f191b81e1cb6792e97b7b98b3358cdb3
zhangbao <1126976340@qq.com>
2018-02-03 22:39:08 +0800

    新增三个unit test

--- Assignment0.1Tests/Assignment0_1Tests.swift
+++ Assignment0.1Tests/Assignment0_1Tests.swift
@@ -19,7 +19,43 @@ class Assignment0_1Tests: XCTestCase {
         queryWeather(latitude: 39.92642,
                      longitude: 116.447591) { (response) in
                 expectation.fulfill()
-                assert(response != nil, "Weather api request fail!")
+                XCTAssert(response != nil, "Weather api request fail!")
+        }
+        self.waitForExpectations(timeout: timeout, handler: nil)
+    }
+    
+    func testWeatherDesc() {
+        let expectation = self.expectation(description: "")
+        let timeout = 15 as TimeInterval
+        queryWeather(latitude: 39.92642,
+                     longitude: 116.447591) { (response) in
+                        expectation.fulfill()
+                        XCTAssert(response != nil, "Weather api request fail!")
+                        let arr = response?.object(forKey: "weather") as! NSArray
+                        XCTAssert(arr.count > 0, "Weather data is null!")
+                        let weather = arr.firstObject as! NSDictionary
+                        let desc = weather.object(forKey: "description") as! NSString
+                        XCTAssert(desc.length > 0, "Weather description is null!")
+        }
+        self.waitForExpectations(timeout: timeout, handler: nil)
+    }
+    
+    func testEventObjectDateToString() {
+        let eventObj = EventObject()
+        XCTAssert(eventObj.dateToString() == "", "EventObject dateToString fail!")
+    }
+    
+    func testWeatherTemp() {
+        let expectation = self.expectation(description: "")
+        let timeout = 15 as TimeInterval
+        queryWeather(latitude: 39.92642,
+                     longitude: 116.447591) { (response) in
+                        expectation.fulfill()
+                        XCTAssert(response != nil, "Weather api request fail!")
+                        let arr = response?.object(forKey: "weather") as! NSArray
+                        XCTAssert(arr.count > 0, "Weather data is null!")
+                        let main = response?.object(forKey: "main") as! NSDictionary
+                        XCTAssert(main.object(forKey: "temp") != nil, "Get temp fail!")
         }
         self.waitForExpectations(timeout: timeout, handler: nil)
     }
